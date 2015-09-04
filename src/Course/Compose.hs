584e7a8fcd05618af53d652017d512e2e0ecf768
M.Magomedov <mmagomedoff@gmail.com>
2015-09-04 23:27:41 +0300

    Compose exercises

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -11,29 +11,23 @@ import Course.Bind
 
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
-newtype Compose f g a =
-  Compose (f (g a))
+newtype Compose f g a = Compose (f (g a))
 
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  -- (<$>) :: (a -> b) -> Compose f g a -> Compose.hs f g b
+  (<$>) f (Compose c) = Compose $ (\g -> f <$> g) <$> c
 
-instance (Apply f, Apply g) =>
-  Apply (Compose f g) where
--- Implement the (<*>) function for an Apply instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+instance (Apply f, Apply g) => Apply (Compose f g) where
+  -- (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  -- Compose f <*> Compose v = Compose (lift2 (<*>) f v)
+  Compose op <*> Compose val = let func = ((\x -> (x <*> )) <$> op)
+                               in Compose $ func <*> val
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure a = Compose (pure (pure a))
 
-instance (Bind f, Bind g) =>
-  Bind (Compose f g) where
--- Implement the (=<<) function for a Bind instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+instance (Bind f, Bind g) => Bind (Compose f g) where
+  -- Implement the (=<<) function for a Bind instance for Compose
+  (=<<) = error "impossible"
