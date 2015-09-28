ea61f31f0a708ee8d918610c6801c35b9327c83b
M.Magomedov <mmagomedoff@gmail.com>
2015-09-29 02:15:14 +0300

    FastAnagrams exercise

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -6,12 +6,17 @@ module Course.FastAnagrams where
 import Course.Core
 import Course.List
 import Course.Functor
+import Course.Applicative
 import qualified Data.Set as S
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
 fastAnagrams :: Chars -> Filename -> IO (List Chars)
-fastAnagrams = error "todo: Course.FastAnagrams#fastAnagrams"
+fastAnagrams word filename = do
+  content <- readFile filename
+  let dictionary = S.fromList $ hlist $ lines content
+  let annagrams = S.fromList $ hlist $ permutations word
+  return $ listh $ S.toList $ S.intersection annagrams dictionary
 
 newtype NoCaseString = NoCaseString {
     ncString :: Chars
